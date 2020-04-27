9c882edcd37f2b3155a7ca2581890c83a7dd6bcd
Manuel (Aesthetical) <AestheticalGames@users.noreply.github.com>
2020-04-27 20:19:44 +0200

    Restructured bblaunch

--- bblaunch/bblaunch.cpp
+++ bblaunch/bblaunch.cpp
@@ -7,22 +7,6 @@ using namespace std;
 static const char* bb_err =
 "Error when creating Blitz3D process.";
 
-
-/*static string getAppDir()  --OLD GETAPPDIR
-{
-	char buff[MAX_PATH];
-	if(GetModuleFileName(0, buff, MAX_PATH))
-	{
-		string t = buff;
-		int n = t.find_last_of('\\');
-		if(n != string::npos) t = t.substr(0, n);
-		return t;
-	}
-	return "";
-}
-*/
-
-//Made it more compact.
 static string getAppDir()
 {
 	char buffer[MAX_PATH];
@@ -43,25 +27,28 @@ int _stdcall WinMain(HINSTANCE inst, HINSTANCE prev, char* cmd, int show)
 	//We really need a better way to get the application path...
 	//Maybe with C++17's new std::filesystem::path class?
 	string t = getAppDir();
+	string idePath = "\\bin\\ide.exe ";
+
 	putenv(("blitzpath=" + t).c_str());
 	SetCurrentDirectory(t.c_str());
-	t = t + "\\bin\\ide.exe " + cmd;
+	t += idePath + cmd;
 
-	STARTUPINFO si;
-	PROCESS_INFORMATION pi;
-	ZeroMemory(&si, sizeof(si)); si.cb = sizeof(si);
+	STARTUPINFO startupInfo;
+	PROCESS_INFORMATION processInfo;
+	ZeroMemory(&startupInfo, sizeof(startupInfo)); startupInfo.cb = sizeof(startupInfo);
 
-	if(!CreateProcess(0, (char*)t.c_str(), 0, 0, 0, 0, 0, 0, &si, &pi))
+	if(!CreateProcess(0, (char*)t.c_str(), 0, 0, 0, 0, 0, 0, &startupInfo, &processInfo))
 	{
+		//Crash the application.
 		fail(bb_err);
 	}
 
-	//wait for BB to start
-	WaitForInputIdle(pi.hProcess, INFINITE);
+	//Wait for BB to start...
+	WaitForInputIdle(processInfo.hProcess, INFINITE);
 
-	// Close process and thread handles. 
-	CloseHandle(pi.hProcess);
-	CloseHandle(pi.hThread);
+	//Close process and thread handles. 
+	CloseHandle(processInfo.hProcess);
+	CloseHandle(processInfo.hThread);
 
-	return 0;
+	return 0; //Success
 }
