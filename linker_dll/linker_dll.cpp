fbf4a84633a188ff5c175f0653c5ee5859b68c77
AestheticalZ <51166756+AestheticalZ@users.noreply.github.com>
2022-02-10 01:34:01 +0100

    Format linker_dll.

--- linker_dll/linker_dll.cpp
+++ linker_dll/linker_dll.cpp
@@ -4,17 +4,17 @@
 
 #include <Windows.h>
 
-BOOL APIENTRY DllMain(HANDLE hModule,DWORD ul_reason_for_call,LPVOID lpReserved){
+BOOL APIENTRY DllMain(HANDLE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
 
-    switch( ul_reason_for_call ){
-    case DLL_PROCESS_ATTACH:
-		linkerGetLinker();
-		break;
-    case DLL_THREAD_ATTACH:
-    case DLL_THREAD_DETACH:
-    case DLL_PROCESS_DETACH:
-	default:
-		;
-    }
-    return TRUE;
+	switch(ul_reason_for_call) {
+		case DLL_PROCESS_ATTACH:
+			linkerGetLinker();
+			break;
+		case DLL_THREAD_ATTACH:
+		case DLL_THREAD_DETACH:
+		case DLL_PROCESS_DETACH:
+		default:
+			;
+	}
+	return TRUE;
 }
