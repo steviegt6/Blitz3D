79564d0d09ef5ea9ba9f8372b2c096555c886dda
子悦解说 <ziyuecommentary@gmail.com>
2024-04-14 11:05:10 +0800

    Update to v1.132 & update copyright

--- blitz/main.cpp
+++ blitz/main.cpp
@@ -32,7 +32,7 @@ static std::string verstr(int ver) {
 static void showInfo() {
 	std::cout << std::endl;
 	std::cout << "BlitzCC v" << verstr(VERSION) << std::endl;
-	std::cout << "(C)opyright 2000-2003 Blitz Research Ltd, 2022 Third Subdivision Studios, 2023 ZiYueCommentary" << std::endl;
+	std::cout << "(C)opyright 2000-2003 Blitz Research Ltd, 2022 Third Subdivision Studios, 2023-2024 ZiYueCommentary" << std::endl;
 	std::cout << "------------------------------------------------------------------------" << std::endl;
 }
 
