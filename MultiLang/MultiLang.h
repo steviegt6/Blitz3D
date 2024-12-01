0a48f4dac87cf1a52628478e15618e51f00ea9fb
子悦解说 <ziyuecommentary@gmail.com>
2024-12-01 14:17:40 +0800

    Optimized MultiLang

--- MultiLang/MultiLang.h
+++ MultiLang/MultiLang.h
@@ -2,8 +2,6 @@
 #define MULTILANG
 
 namespace MultiLang {
-	typedef const char* string;
-	typedef const wchar_t* wstring;
 #ifdef CHINESE_SIMP // Simplified Chinese
 	#include "SimplifiedChinese.cpp"
 #elif defined CHINESE_TRAD // Traditional Chinese
